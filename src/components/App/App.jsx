b05b7f0bf3a6daaf4a8363c81a3e60b543c0be7d
Juan Villegas <jvillegas422@gmail.com>
2022-06-12 13:17:04 -0500

    Added POST to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,27 +5,41 @@ import GalleryList from '../GalleryList/GalleryList';
 import './App.css';
 
 function App() {
-  let [galleryList, setGalleryList] = useState([]);
+  let [galleryItems, setGalleryItems] = useState([]);
 
   // Runs when the component is first put on the DOM
   useEffect(() => {
     getGallery();
   });
 
-  // GET route
-  const fetchGalleryItem = () => {
-    axios({
-      method: 'GET',
-      url: '/gallery',
-    })
-      .then((response) => {
-        setGalleryItem(response);
-        console.log('Gallery items:', response);
-      })
-        .catch((err) => {
-          console.log('error in GET', err);
-        });
-  };
+// GET data from server
+const fetchGalleryItem = () => {
+  axios({
+    method: 'GET',
+    url: '/gallery',
+  })
+    .then((response) => {
+      setGalleryItems(response);
+      console.log('Gallery items:', response);
+  })
+    .catch((err) => {
+      console.log('error in GET', err);
+  });
+};
+
+// Post to server
+const addGalleryItem = (newGalleryItemInput) => {
+  axios
+    .post('/gallery', newGalleryItemInput)
+    .then((response) => {
+      console.log('In POST /gallery', response);
+      fetchGalleryItem();
+  })
+    .catch((err) => {
+      alert('Error, unable to add item.');
+      console.log('err');
+  });
+};
 
     return (
       <div className="App">
